677a943a565b0d364774954f1d8ae48209919f25
Iain MacDonald <IJMacD@gmail.com>
2019-10-10 23:10:21 +0800

    Add more legal moves

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { makeBoard, fileNumberToName, pieceNameToSymbol, rankNameToNumber, fileNameToNumber, rankNumberToName, symbolToColour, pieceSymbolToName, positionFromNames, positionFromNumbers } from './chess';
+import { makeBoard, fileNumberToName, movePiece } from './chess';
 
 const MOVE_HISTORY = "chess_move_history";
 
@@ -13,40 +13,45 @@ export default class App extends React.Component {
     const moves = localStorage.getItem(MOVE_HISTORY) || "";
 
     this.state = {
-      board: this.doMoves(moves),
+      board: makeBoard(),
       moves,
       error: null,
     };
+
+    setTimeout(() => this.doMoves(moves), 10);
   }
 
   doMoves (moves) {
-    const re = /^\d+\. *([^ ]+)(?: +([^ ]+))?/
-    const moveList = moves.split("\n").filter(m => re.test(m));
-
+    const re = /\d+\. *([^ ]+)(?: +([^ ]+))?/g
     const board = makeBoard();
 
+    let move;
+    let i = 1;
+
     try {
-      for (const move of moveList) {
-        const [ _, whiteMove, blackMove] = re.exec(move);
+      while (move = re.exec(moves)) {
+        const [ _, whiteMove, blackMove] = move;
 
         movePiece(board, whiteMove, false);
 
         if (blackMove) {
           movePiece(board, blackMove, true);
         }
+
+        i++;
       }
     } catch (error) {
+      error.message = `Move ${i} - ${error.message}`;
       this.setState({ error });
     }
 
-    return board;
+    this.setState({ board });
   }
 
   componentDidUpdate (prevProps, prevState) {
     localStorage.setItem(MOVE_HISTORY, this.state.moves);
     if (this.state.moves !== prevState.moves) {
-      const board = this.doMoves(this.state.moves);
-      this.setState({ board });
+      this.doMoves(this.state.moves);
     }
   }
 
@@ -74,155 +79,4 @@ export default class App extends React.Component {
       </div>
     );
   }
-}
-
-
-
-const reMove = /([KQBNR]?)([a-h]?)([1-8]?)([a-h])([1-8])/;
-function movePiece (board, move, isBlack=false) {
-  if (reMove.test(move)) {
-    let [_, piece, fromFileName, fromRankName, toFileName, toRankName] = reMove.exec(move);
-
-    const symbol = pieceNameToSymbol(piece, isBlack);
-
-    let fromPos;
-    const toPos = positionFromNames(toFileName, toRankName);
-
-    if (fromFileName && fromRankName) {
-      fromPos = positionFromNames(fromFileName, fromRankName);
-
-      if (!isValidMove(board, fromPos, toPos)) {
-        throw Error(`Invalid Move: ${symbol}: ${fromFileName}${fromRankName} -> ${toFileName}${toRankName}`);
-      }
-    } else {
-      fromPos = findPieces(board, symbol, fromFileName, fromRankName).find(p => isValidMove(board, p, toPos));
-
-      if (!fromPos) {
-        throw Error(`Invalid Move: ${symbol}: -> ${toFileName}${toRankName}`);
-      }
-
-      fromFileName = fromPos.fileName;
-      fromRankName = fromPos.rankName;
-    }
-
-    board[fromPos.rankNumber][fromPos.fileNumber] = '';
-    board[toPos.rankNumber][toPos.fileNumber] = symbol;
-  }
-}
-
-/**
- *
- * @param {string[][]} board
- * @param {string} symbol
- * @returns {Position[]}
- */
-function findPieces (board, symbol, fileName="", rankName="") {
-  const out = [];
-
-  if (fileName && rankName) {
-    const pos = positionFromNames(fileName, rankName);
-
-    if (board[pos.rankNumber][pos.fileNumber]) {
-      out.push(pos);
-    }
-  } else if (fileName) {
-    const fileNumber = fileNameToNumber(fileName);
-
-    for (let rankNumber = 0; rankNumber < 8; rankNumber++) {
-      if (board[rankNumber][fileNumber] === symbol) {
-        out.push(positionFromNumbers(fileNumber, rankNumber));
-      }
-    }
-  } else if (rankName) {
-    const rankNumber = rankNameToNumber(rankName);
-
-    for (let fileNumber = 0; fileNumber < 8; fileNumber++) {
-      if (board[rankNumber][fileNumber] === symbol) {
-        out.push(positionFromNumbers(fileNumber, rankNumber));
-      }
-    }
-  }
-  else {
-    for (let rankNumber = 0; rankNumber < 8; rankNumber++) {
-      for (let fileNumber = 0; fileNumber < 8; fileNumber++) {
-        if (board[rankNumber][fileNumber] === symbol) {
-          out.push(positionFromNumbers(fileNumber, rankNumber));
-        }
-      }
-    }
-  }
-
-  return out;
-}
-
-/**
- *
- * @param {string[][]} board
- * @param {Position} from
- * @param {Position} to
- */
-function isValidMove (board, from, to) {
-  const fromPieceSymbol = board[from.rankNumber][from.fileNumber];
-  const toPieceSymbol = board[to.rankNumber][to.fileNumber];
-
-  if (!fromPieceSymbol) return false;
-
-  const fromColour = symbolToColour(fromPieceSymbol);
-  const toColour = symbolToColour(toPieceSymbol);
-
-  if (fromColour === toColour) return false;
-
-  const fromPieceName = pieceSymbolToName(fromPieceSymbol);
-
-  const deltaRank = to.rankNumber - from.rankNumber;
-  const deltaFile = to.fileNumber - from.fileNumber;
-  const absDeltaRank = Math.abs(deltaRank);
-  const absDeltaFile = Math.abs(deltaFile);
-
-  switch (fromPieceName) {
-    //pawn
-    case "": {
-      // When taking file must be different
-      if (toPieceSymbol) {
-        return absDeltaFile === 1 && absDeltaRank === 1;
-      }
-
-      // Unless taking a piece pawns must remain in the same file
-      if (from.fileName !== to.fileName) return false;
-
-      if (fromColour === "white") {
-        if (from.rankName === "2" && deltaRank === -2) return true;
-        return deltaRank === -1 && Math.abs(deltaFile) <= 1;
-      } else if (fromColour === "black") {
-        if (from.rankName === "7" && deltaRank === 2) return true;
-        return deltaRank === 1 && Math.abs(deltaFile) <= 1;
-      }
-
-      break;
-    }
-    // Queen
-    case "Q": {
-      return isPathClear(board, from, to) && (deltaRank === 0 || deltaFile === 0 || absDeltaRank === absDeltaFile);
-    }
-    // Knight
-    case "N": {
-      return (absDeltaFile === 2 && absDeltaRank === 1) || (absDeltaFile === 1 && absDeltaRank === 2);
-    }
-    // Rook
-    case "R": {
-      return isPathClear(board, from, to) && (deltaRank === 0 || deltaFile === 0);
-    }
-    // King
-    case "K": {
-      return absDeltaRank <= 1 && absDeltaFile <= 1;
-    }
-    // Bishop
-    case "B": {
-      return isPathClear(board, from, to) && absDeltaRank === absDeltaFile;
-    }
-  }
-}
-
-function isPathClear (board, from, to) {
-  return true;
 }
--- src/chess.js
+++ src/chess.js
@@ -1,4 +1,3 @@
-
 /**
  * @typedef Position
  * @prop {string} fileName
@@ -116,4 +115,250 @@ export function positionFromNumbers (fileNumber, rankNumber) {
         fileName: fileNumberToName(fileNumber),
         rankName: rankNumberToName(rankNumber),
     };
+}
+
+const reMove = /([KQBNR]?)([a-h]?)([1-8]?)x?([a-h])([1-8])(?:=([QBNR]))?/;
+export function movePiece (board, move, isBlack=false) {
+
+    if (move === "O-O") {
+        if (isBlack) {
+            if (board[0][4] === '♛' &&
+                board[0][5] === '' &&
+                board[0][6] === '' &&
+                board[0][7] === '♜'
+            ) {
+                board[0][4] = '';
+                board[0][5] = '♜';
+                board[0][6] = '♛';
+                board[0][7] = '';
+            } else {
+                throw Error("Illegal Castling")
+            }
+        }
+        else {
+            if (board[7][4] === '♕' &&
+                board[7][5] === '' &&
+                board[7][6] === '' &&
+                board[7][7] === '♖'
+            ) {
+                board[7][4] = '';
+                board[7][5] = '♖';
+                board[7][6] = '♕';
+                board[7][7] = '';
+            } else {
+                throw Error("Illegal Castling")
+            }
+        }
+
+        return;
+    }
+
+    if (move === "O-O-O") {
+        if (isBlack) {
+            if (
+                board[0][0] === '♜' &&
+                board[0][1] === '' &&
+                board[0][2] === '' &&
+                board[0][3] === '' &&
+                board[0][4] === '♛'
+            ) {
+                board[0][0] = '';
+                board[0][2] = '♛';
+                board[0][3] = '♜';
+                board[0][4] = '';
+            } else {
+                throw Error("Illegal Castling")
+            }
+        }
+        else {
+            if (
+                board[7][0] === '♖' &&
+                board[7][1] === '' &&
+                board[7][2] === '' &&
+                board[7][3] === '' &&
+                board[7][4] === '♕'
+            ) {
+                board[7][0] = '';
+                board[7][2] = '♕';
+                board[7][3] = '♖';
+                board[7][4] = '';
+            } else {
+                throw Error("Illegal Castling")
+            }
+        }
+
+        return;
+    }
+
+    if (reMove.test(move)) {
+        let [_, piece, fromFileName, fromRankName, toFileName, toRankName, promotion] = reMove.exec(move);
+
+        let symbol = pieceNameToSymbol(piece, isBlack);
+
+        let fromPos;
+        const toPos = positionFromNames(toFileName, toRankName);
+
+        if (fromFileName && fromRankName) {
+            fromPos = positionFromNames(fromFileName, fromRankName);
+
+            if (!isValidMove(board, fromPos, toPos)) {
+                throw Error(`Invalid Move: ${symbol}: ${fromFileName}${fromRankName} -> ${toFileName}${toRankName}`);
+            }
+        } else {
+            fromPos = findPieces(board, symbol, fromFileName, fromRankName).find(p => isValidMove(board, p, toPos));
+
+            if (!fromPos) {
+                throw Error(`Invalid Move: ${symbol}: -> ${toFileName}${toRankName}`);
+            }
+
+            fromFileName = fromPos.fileName;
+            fromRankName = fromPos.rankName;
+        }
+
+        if (promotion) {
+            if (isBlack && piece === "" && toPos.rankName === "1") {
+                symbol = pieceNameToSymbol(promotion, true);
+            } else if (!isBlack && piece === "" && toPos.rankName === "8") {
+                symbol = pieceNameToSymbol(promotion, false);
+            } else {
+                throw Error("Illegal promotion");
+            }
+        }
+
+        board[fromPos.rankNumber][fromPos.fileNumber] = '';
+        board[toPos.rankNumber][toPos.fileNumber] = symbol;
+    }
+}
+
+/**
+ *
+ * @param {string[][]} board
+ * @param {string} symbol
+ * @returns {Position[]}
+ */
+function findPieces (board, symbol, fileName="", rankName="") {
+    const out = [];
+
+    if (fileName && rankName) {
+        const pos = positionFromNames(fileName, rankName);
+
+        if (board[pos.rankNumber][pos.fileNumber]) {
+        out.push(pos);
+        }
+    } else if (fileName) {
+        const fileNumber = fileNameToNumber(fileName);
+
+        for (let rankNumber = 0; rankNumber < 8; rankNumber++) {
+        if (board[rankNumber][fileNumber] === symbol) {
+            out.push(positionFromNumbers(fileNumber, rankNumber));
+        }
+        }
+    } else if (rankName) {
+        const rankNumber = rankNameToNumber(rankName);
+
+        for (let fileNumber = 0; fileNumber < 8; fileNumber++) {
+        if (board[rankNumber][fileNumber] === symbol) {
+            out.push(positionFromNumbers(fileNumber, rankNumber));
+        }
+        }
+    }
+    else {
+        for (let rankNumber = 0; rankNumber < 8; rankNumber++) {
+        for (let fileNumber = 0; fileNumber < 8; fileNumber++) {
+            if (board[rankNumber][fileNumber] === symbol) {
+            out.push(positionFromNumbers(fileNumber, rankNumber));
+            }
+        }
+        }
+    }
+
+    return out;
+}
+
+/**
+ *
+ * @param {string[][]} board
+ * @param {Position} from
+ * @param {Position} to
+ */
+function isValidMove (board, from, to) {
+    const fromPieceSymbol = board[from.rankNumber][from.fileNumber];
+    const toPieceSymbol = board[to.rankNumber][to.fileNumber];
+
+    if (!fromPieceSymbol) return false;
+
+    const fromColour = symbolToColour(fromPieceSymbol);
+    const toColour = symbolToColour(toPieceSymbol);
+
+    if (fromColour === toColour) return false;
+
+    const fromPieceName = pieceSymbolToName(fromPieceSymbol);
+
+    const deltaRank = to.rankNumber - from.rankNumber;
+    const deltaFile = to.fileNumber - from.fileNumber;
+    const absDeltaRank = Math.abs(deltaRank);
+    const absDeltaFile = Math.abs(deltaFile);
+
+    switch (fromPieceName) {
+        //pawn
+        case "": {
+            // When taking file must be different
+            if (toPieceSymbol) {
+                return absDeltaFile === 1 && absDeltaRank === 1;
+            }
+
+            // Unless taking a piece pawns must remain in the same file
+            if (from.fileName !== to.fileName) return false;
+
+            if (fromColour === "white") {
+                if (from.rankName === "2" && deltaRank === -2) return true;
+                return deltaRank === -1 && Math.abs(deltaFile) <= 1;
+            } else if (fromColour === "black") {
+                if (from.rankName === "7" && deltaRank === 2) return true;
+                return deltaRank === 1 && Math.abs(deltaFile) <= 1;
+            }
+
+            break;
+        }
+        // Queen
+        case "Q": {
+            return isPathClear(board, from, to) && (deltaRank === 0 || deltaFile === 0 || absDeltaRank === absDeltaFile);
+        }
+        // Knight
+        case "N": {
+            return (absDeltaFile === 2 && absDeltaRank === 1) || (absDeltaFile === 1 && absDeltaRank === 2);
+        }
+        // Rook
+        case "R": {
+            return isPathClear(board, from, to) && (deltaRank === 0 || deltaFile === 0);
+        }
+        // King
+        case "K": {
+            return absDeltaRank <= 1 && absDeltaFile <= 1;
+        }
+        // Bishop
+        case "B": {
+            return isPathClear(board, from, to) && absDeltaRank === absDeltaFile;
+        }
+    }
+}
+
+function isPathClear (board, from, to) {
+    const deltaRank = to.rankNumber - from.rankNumber;
+    const deltaFile = to.fileNumber - from.fileNumber;
+
+    const absDeltaRank = Math.abs(deltaRank);
+    const absDeltaFile = Math.abs(deltaFile);
+
+    const rankStep = Math.sign(deltaRank);
+    const fileStep = Math.sign(deltaFile);
+
+    for (let i = 1; i < Math.max(absDeltaFile, absDeltaRank); i++) {
+        const f = from.fileNumber + i * fileStep;
+        const r = from.rankNumber + i * rankStep;
+
+        if (board[r][f] !== '') return false;
+    }
+
+    return true;
 }
